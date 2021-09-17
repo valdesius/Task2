f20b5f9239aa4298b245be66a38f5578cdf27495
valdesius <v.inc@inbox.ru>
2021-09-17 16:25:51 +0300

    V1.03

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,44 @@
 package com.company;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException  {
+        Locale.setDefault(Locale.ROOT);
+        int a = readNumber();
+        printInfo(a);
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Введите число в диапазоне от: -1000 до 1000  - ");
-        int a = Integer.parseInt(bufferedReader.readLine());
+}
 
+    private static int readNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число из списка: ");
+        System.out.println("1. отрицательное нечетное однозначное число" + "\n" + "2. положительное четное трехзначное число " + "\n" + "3. Нулевое число");
+        int num;
 
-        if((a%2)!=0 && a<0 && a > -10 && a<= -1){
+        if (scanner.hasNextInt()) {
+            num = scanner.nextInt();
+        } else {
+            System.out.print("Неправильный формат записи ");
+            return readNumber();
+        }
+        return num;
+    }
+
+    private static void printInfo(int num){
+        if((num%2)!=0 && num<0 && num > -10 && num<= -1){
             System.out.println("отрицательное нечетное однозначное число ");
-        } else if ((a%2)==0 && a > 0 && a >= 100 && a < 1000){
+        } else if ((num%2)==0 && num > 0 && num >= 100 && num < 1000){
             System.out.println("положительное четное трехзначное число ");
 
-        } else if(a == 0){
+        } else if(num == 0){
             System.out.println("Нулевое число");
         }
+    }
 
-
-}}
+}
